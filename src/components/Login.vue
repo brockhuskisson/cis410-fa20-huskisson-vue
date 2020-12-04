cd9735e1591db445a8ad42f05594507b21465d33
brockhuskisson <bhusk@rams.colostate.edu>
2020-12-03 20:19:58 -0700

    13.2 11/19 axios, signup route

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>Login</h1>
+        <div v-if="this.$route.query.signupsuccess" class ="alert alert-success">thanks for signing up, please login now</div>
     </div>
 </template>
 
