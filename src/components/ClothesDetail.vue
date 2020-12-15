2b7bb28f6ffcbab15195bcd994c98e31f1a7cea5
brockhuskisson <bhusk@rams.colostate.edu>
2020-12-14 20:45:58 -0700

    15.2 13/3 redeploy /clothes/:pk

--- src/components/ClothesDetail.vue
+++ src/components/ClothesDetail.vue
@@ -1,13 +1,36 @@
 <template>
     <div>
-        <h1>Clothes Detail
-        </h1>
+       <div class = "card">
+           <div class ="card-body">
+               <h2 class = "text-primary">{{clothes.BrandName}}</h2>
+               <br/>
+               <p>Type: <br/><strong>{{clothes.Type}}</strong></p>
+               <p>Material: <br/><strong>{{clothes.Material}}</strong></p>
+           </div>
+       </div>
+        <br/>
+       <router-link v-if="auth" :to="`/clothes/${this.$route.params.pk}/order`">
+       <button type ="button" class = "btn btn-success">Place an Order</button>
+       </router-link>
+
+       <router-link v-else :to="`/signin`">
+       <button type ="button" class = "btn btn-outline-success">Sign In to Place an Order</button>
+       </router-link>
     </div>
 </template>
 
 <script>
 export default {
-    
+    computed: {
+        clothes(){
+            var clothes = this.$store.state.clothes;
+            var thisClothes = clothes.find((aClothe)=> aClothe.ClothesPK == this.$route.params.pk)
+
+            //console.log("here is the clothe you want", thisClothes)
+            return thisClothes
+        },
+        auth(){return this.$store.state.token}
+    }
 }
 </script>
 
